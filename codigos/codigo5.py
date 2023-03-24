3fa4f779c48caaf323bdf95adca777abe64303d7
AndreOliveiraMendes <ao_mendes@hotmail.com>
2023-03-24 16:31:04 -0300

    Update codigo5.py

--- codigos/codigo5.py
+++ codigos/codigo5.py
@@ -1,3 +1,5 @@
+import pandas as pd
+#gerando os arquivos em txt
 first = True
 with open("resultado1.txt", "w") as arq:
     for t in l1:
@@ -14,3 +16,12 @@ with open("resultado2.txt", "w") as arq:
         else:
             arq.write("\n")
         arq.write(t)
+#gerando os arquivo em excel
+df1 = pd.DataFrame({'posição': list(range(1, len(l1) + 1)),
+                    'Titulo': l1})
+df2 = pd.DataFrame({'posição': list(range(1, len(l2) + 1)),
+                    'Titulo': l2})
+writer = pd.ExcelWriter('result.xlsx', engine='xlsxwriter')
+df1.to_excel(writer, sheet_name='titulos do arquivo.bib', index=False)
+df2.to_excel(writer, sheet_name='titulos do arquivo.txt', index=False)
+writer.close()

39b2ef8f3c83565ba76beecc5d26f9be78621c8c
AndreOliveiraMendes <ao_mendes@hotmail.com>
2023-03-23 21:13:13 -0300

    Update codigo5.py

--- codigos/codigo5.py
+++ codigos/codigo5.py
@@ -1,6 +1,16 @@
+first = True
 with open("resultado1.txt", "w") as arq:
     for t in l1:
+        if first:
+            first = False
+        else:
+            arq.write("\n")
         arq.write(t)
+first = True
 with open("resultado2.txt", "w") as arq:
     for t in l2:
+        if first:
+            first = False
+        else:
+            arq.write("\n")
         arq.write(t)
